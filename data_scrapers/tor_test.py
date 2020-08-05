ad1712bf4e4e6b421a878f2a7d17ea5aa4050a96
BinaryWiz <Ultimi450@gmail.com>
2020-08-05 12:30:48 -0400

    finished testing the tor browser to see if it will work, and it will

--- data_scrapers/tor_test.py
+++ data_scrapers/tor_test.py
@@ -1,42 +1,18 @@
-# from stem import Signal
-# from stem.control import Controller
-# from selenium import webdriver
-# from selenium.webdriver.firefox.options import Options
-# from bs4 import BeautifulSoup
-# from time import sleep
-
-# # signal TOR for a new connection
-# def switchIP():
-#     with Controller.from_port(port = 9051) as controller:
-#         sleep(5)
-#         controller.authenticate()
-#         controller.signal(Signal.NEWNYM)
-
-# # get a new selenium webdriver with tor as the proxy
-# def my_proxy(PROXY_HOST,PROXY_PORT):
-#     fp = webdriver.FirefoxProfile()
-#     # Direct = 0, Manual = 1, PAC = 2, AUTODETECT = 4, SYSTEM = 5
-#     fp.set_preference('general.useragent.override', 'Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0')
-#     fp.set_preference("network.proxy.type", 1)
-#     fp.set_preference("network.proxy.socks",PROXY_HOST)
-#     fp.set_preference("network.proxy.socks_port",int(PROXY_PORT))
-#     fp.update_preferences()
-#     options = Options()
-#     #options.headless = True
-#     return webdriver.Firefox(options=options, firefox_profile=fp)
-
-# proxy = my_proxy("127.0.0.1", 9050)
-# proxy.get("https://pcpartpicker.com/products/cpu/")
-# html = proxy.page_source
-# soup = BeautifulSoup(html, 'lxml')
-# cookies = proxy.get_cookies()
-# for cookie in cookies:
-#     print(cookie)
-# # print(soup.find("span", {"id": "ipv4"}))
-# # print(soup.find("span", {"id": "ipv6"}))
-# # switchIP()
-
+from stem import Signal
+from stem.control import Controller
 from tbselenium.tbdriver import TorBrowserDriver
+from time import sleep
 
-with TorBrowserDriver('/home/jason/.local/share/torbrowser/tbb/x86_64/tor-browser_en-US/') as driver:
-    driver.get('https://www.pcpartpicker.com/')
+# signal TOR for a new connection
+def switchIP():
+    with Controller.from_port(port = 9051) as controller:
+        sleep(5)
+        controller.authenticate()
+        controller.signal(Signal.NEWNYM)
+
+for x in range(5):
+    with TorBrowserDriver('/home/jason/.local/share/torbrowser/tbb/x86_64/tor-browser_en-US/') as driver:
+        driver.get('https://www.pcpartpicker.com/products/cpu/')
+        sleep(15)
+        print(driver.page_source)
+    switchIP()
