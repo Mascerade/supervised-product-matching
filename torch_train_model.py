01c1ceeea8160451ca7d3004bd65730c4edf4007
BinaryWiz <ultimi450@gmail.com>
2021-03-30 02:30:28 -0400

    NOW skipping batch. didn't get rid of while before

--- torch_train_model.py
+++ torch_train_model.py
@@ -138,50 +138,48 @@ def validation(data, labels, name):
             batch_data = data[position:position + VAL_BATCH_SIZE]
             batch_labels = labels[position:position + VAL_BATCH_SIZE]
 
-        while True:
-            try:
-                # Forward propagation
-                loss, forward = forward_prop(batch_data, batch_labels, net, criterion)
-                
-                # Get the predictions from the net
-                y_pred = torch.argmax(forward, dim=1).cpu()
-
-                # Calculate accuracy
-                accuracy = np.sum(y_pred.detach().numpy() == batch_labels) / float(batch_labels.shape[0])
-
-                # Get the confusion matrix and calculate precision, recall and F1 score
-                confusion = confusion_matrix(batch_labels, y_pred.detach().numpy(), labels=[0, 1])
-                tn, fp, fn, tp = confusion.ravel()
-                running_tn += tn
-                running_fp += fp
-                running_fn += fn
-                running_tp += tp
-                precision = tp / (tp + fp)
-                recall = tp / (tp + fn)
-                f1_score = 2 * ((precision * recall) / (precision + recall))
-
-                # Add to running loss and accuracy (every 10 batches)
-                running_loss += loss.item()
-                running_accuracy += accuracy
-                
-                # Print statistics every batch
-                print("Torch memory allocator: {} bytes".format(torch.cuda.memory_reserved()))
-                print('%s Batch: %5d, Loss: %.6f, Accuracy: %.6f, Running Loss: %.6f, Running Accuracy: %.6f, Precision: %.3f, Recall: %.3f, F1 Score: %.3f' %
-                        (name, i + 1, loss, accuracy, running_loss / current_batch, running_accuracy / current_batch, precision, recall, f1_score))
-
-                # Clear our running variables every 10 batches
-                if (current_batch == PERIOD):
-                    current_batch = 0
-                    running_loss = 0
-                    running_accuracy = 0
-                
-                break
+        try:
+            # Forward propagation
+            loss, forward = forward_prop(batch_data, batch_labels, net, criterion)
             
-            except RuntimeError as e:
-                if "out of memory" in str(e):
-                    print("WARNING: Ran out of memory. Skipping Batch.")
-                    gc.collect()
-                    torch.cuda.empty_cache()
+            # Get the predictions from the net
+            y_pred = torch.argmax(forward, dim=1).cpu()
+
+            # Calculate accuracy
+            accuracy = np.sum(y_pred.detach().numpy() == batch_labels) / float(batch_labels.shape[0])
+
+            # Get the confusion matrix and calculate precision, recall and F1 score
+            confusion = confusion_matrix(batch_labels, y_pred.detach().numpy(), labels=[0, 1])
+            tn, fp, fn, tp = confusion.ravel()
+            running_tn += tn
+            running_fp += fp
+            running_fn += fn
+            running_tp += tp
+            precision = tp / (tp + fp)
+            recall = tp / (tp + fn)
+            f1_score = 2 * ((precision * recall) / (precision + recall))
+
+            # Add to running loss and accuracy (every 10 batches)
+            running_loss += loss.item()
+            running_accuracy += accuracy
+            
+            # Print statistics every batch
+            #print("Torch memory allocator: {} bytes".format(torch.cuda.memory_reserved()))
+            print('%s Batch: %5d, Loss: %.6f, Accuracy: %.6f, Running Loss: %.6f, Running Accuracy: %.6f, Precision: %.3f, Recall: %.3f, F1 Score: %.3f' %
+                    (name, i + 1, loss, accuracy, running_loss / current_batch, running_accuracy / current_batch, precision, recall, f1_score))
+
+            # Clear our running variables every 10 batches
+            if (current_batch == PERIOD):
+                current_batch = 0
+                running_loss = 0
+                running_accuracy = 0
+            
+        
+        except RuntimeError as e:
+            if "out of memory" in str(e):
+                print("WARNING: Ran out of memory. Skipping Batch.")
+                gc.collect()
+                torch.cuda.empty_cache()
     
     # Get the statistics for the whole data
     final_precision = running_tp / (running_tp + running_fp)
@@ -207,48 +205,46 @@ for epoch in range(10):
             batch_data = train_data[position:position + BATCH_SIZE]
             batch_labels = train_labels[position:position + BATCH_SIZE]
         
-        while True:
-            try:
-                # Zero the parameter gradients
-                opt.zero_grad()
-                
-                # Forward propagation
-                loss, forward = forward_prop(batch_data, batch_labels, net, criterion)
+        try:
+            # Zero the parameter gradients
+            opt.zero_grad()
+            
+            # Forward propagation
+            loss, forward = forward_prop(batch_data, batch_labels, net, criterion)
 
-                # Calculate accuracy
-                accuracy = np.sum(torch.argmax(forward, dim=1).cpu().detach().numpy() == batch_labels) / float(forward.size()[0])
+            # Calculate accuracy
+            accuracy = np.sum(torch.argmax(forward, dim=1).cpu().detach().numpy() == batch_labels) / float(forward.size()[0])
 
-                # Add to both the running accuracy and running loss (every 10 batches)
-                running_accuracy += accuracy
-                running_loss += loss.item()
+            # Add to both the running accuracy and running loss (every 10 batches)
+            running_accuracy += accuracy
+            running_loss += loss.item()
 
-                # Backprop
-                loss.backward()
+            # Backprop
+            loss.backward()
 
-                # Clip the gradient to minimize chance of exploding gradients
-                torch.nn.utils.clip_grad_norm_(net.parameters(), 0.01)
+            # Clip the gradient to minimize chance of exploding gradients
+            torch.nn.utils.clip_grad_norm_(net.parameters(), 0.01)
 
-                # Apply the gradients
-                opt.step()
+            # Apply the gradients
+            opt.step()
 
-                # Print statistics every batch
-                #print("Torch memory allocator: {} bytes".format(torch.cuda.memory_reserved()))
-                print('Training Epoch: %d, Batch %5d, Loss: %.6f, Accuracy: %.6f, Running Loss: %.6f, Running Accuracy %.6f' %
-                        (epoch + 1, i + 1, loss, accuracy, running_loss / current_batch, running_accuracy / current_batch))
-                
-                # Clear our running variables every 10 batches
-                if (current_batch == PERIOD):
-                    current_batch = 0
-                    running_loss = 0
-                    running_accuracy = 0
+            # Print statistics every batch
+            #print("Torch memory allocator: {} bytes".format(torch.cuda.memory_reserved()))
+            print('Training Epoch: %d, Batch %5d, Loss: %.6f, Accuracy: %.6f, Running Loss: %.6f, Running Accuracy %.6f' %
+                    (epoch + 1, i + 1, loss, accuracy, running_loss / current_batch, running_accuracy / current_batch))
+            
+            # Clear our running variables every 10 batches
+            if (current_batch == PERIOD):
+                current_batch = 0
+                running_loss = 0
+                running_accuracy = 0
 
-                break
-                        
-            except RuntimeError as e:
-                if "out of memory" in str(e):
-                    print("WARNING: Ran out of memory. Skipping Batch.")
-                    gc.collect()
-                    torch.cuda.empty_cache()
+
+        except RuntimeError as e:
+            if "out of memory" in str(e):
+                print("WARNING: Ran out of memory. Skipping Batch.")
+                gc.collect()
+                torch.cuda.empty_cache()
 
     torch.save(net.state_dict(), 'models/{}/{}.pt'.format(FOLDER, MODEL_NAME + '_epoch' + str(epoch + 1)))
 
