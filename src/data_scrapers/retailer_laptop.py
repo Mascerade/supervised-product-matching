216297ab16062342e982e73d38a6230dac0d1ced
BinaryWiz <ultimi450@gmail.com>
2021-03-31 20:08:47 -0400

    able to get 1000 walmart titles

--- src/data_scrapers/retailer_laptop.py
+++ src/data_scrapers/retailer_laptop.py
@@ -10,7 +10,7 @@ import time
 import random
 from pcpartpicker import switchIP
 
-def laptop_title_collector():
+def amazon_laptop_title_collector():
     column_names = ['title']
     file_path = 'data/base/amazon_laptop_titles.csv'
     pages = 20
@@ -39,5 +39,32 @@ def laptop_title_collector():
         
     df.to_csv('/home/jason/Documents/Supervised-Product-Similarity/' + file_path)
 
+def walmart_laptop_collector():
+    column_names = ['title']
+    if not os.path.exists('data/base/walmart_laptop_titles.csv'):
+        df = pd.DataFrame(columns = column_names)
+    
+    else:
+        df = pd.read_csv('data/base/walmart_laptop_titles.csv')
+    
+    for page in range(25):
+        driver = webdriver.Chrome()
+        driver.get('https://www.walmart.com/search/?page={}&query=laptops'.format(page + 1))
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+        time.sleep(random.randint(8, 12))
+        driver.quit()
+
+        for product in soup.find_all('a', attrs={'class': 'product-title-link line-clamp line-clamp-2 truncate-title'}):
+            try:
+                title = product.find('span').text
+                print("Title: {}".format(title))
+                df = df.append(pd.DataFrame([[title]], columns=column_names))
+            
+            except AttributeError as e:
+                print(str(e))
+        
+        df.to_csv('../../data/base/walmart_laptop_titles.csv')
+        time.sleep(random.randint(5, 10))
+
 if __name__ == "__main__":
-    laptop_title_collector()
+    walmart_laptop_collector()
